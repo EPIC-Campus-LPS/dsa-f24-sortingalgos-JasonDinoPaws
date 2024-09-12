0af97183c0be117f3beb271f359a628174088966
JasonDinoPaws <87725873+JasonDinoPaws@users.noreply.github.com>
2024-09-12 11:31:48 -0600

    Update QuadSorts.java

--- src/QuadSorts.java
+++ src/QuadSorts.java
@@ -1,18 +1,51 @@
 public class QuadSorts {
 
     public static int[] bubbleSort(int[] arr){
-        return new int[]{0};
+        boolean sorted = false;
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i]+" | ");
+        System.out.println();
+
+        //while (!sorted)
+       // {
+            int p = 0;
+            int num = arr[p];
+            sorted = true;
+
+            for (int i = 0; i < arr.length; i++)
+            {
+                if (!(arr[p] < arr[i]))
+                {
+                    arr[p] = arr[i];
+                    arr[i+1] = num;
+                    p = i+1;
+                }
+                else
+                    break;
+                for (int j = 0; j < arr.length; j++)
+                    System.out.print(arr[j]+" | ");
+                System.out.println();
+            }
+
+        //}
+        return arr;
     }
 
     public static int[] selectionSort(int[] arr){
-        return new int[]{0};
+        int[] arrfin = new int[arr.length];
+
+        return arrfin;
     }
 
     public static int[] insertionSort(int[] arr){
-        return new int[]{0};
+        int[] arrfin = new int[arr.length];
+
+        return arrfin;
     }
 
     public static int[] mergeSort(int[] arr){
-        return new int[]{0};
+        int[] arrfin = new int[arr.length];
+
+        return arrfin;
     }
 }
